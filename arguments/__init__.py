b3bdc8ceebdd823bca58983204db34937918c555
paul.erpenstein@gmail.com <paul.erpenstein@gmail.com>
2024-10-17 18:08:10 +0200

    Try to fix group params bug

--- arguments/__init__.py
+++ arguments/__init__.py
@@ -54,6 +54,7 @@ class ModelParams(ParamGroup):
         self._white_background = False
         self.data_device = "cuda"
         self.eval = False
+        self.n_start_gaussians = None
         super().__init__(parser, "Loading Parameters", sentinel)
 
     def extract(self, args):
